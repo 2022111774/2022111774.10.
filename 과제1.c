d968425d9e0a7ce9ae01198ab0c1a5f3c2321a79
2022111774 <115625599+2022111774@users.noreply.github.com>
2022-11-15 21:56:19 +0900

    Add files via upload

---
+++ 과제1.c
@@ -0,0 +1,40 @@
+////과제 1번
+//#include <stdio.h>
+//
+//void sum(int A[5], int B[5]); // 두 일차 배열의 합을 구하기 위한 함수.
+//void mul(int A[5], int B[5]); // 두 일차 배열의 곱을 구하기 위한 함수.
+//
+//int main(void)
+//{
+//    int A[5] = { 1, 2, 3, 4, 5 };
+//    int B[5] = { 5, 4, 3, 2, 1 };
+//    sum(A, B);
+//    mul(A, B);
+//    return 0;
+//}
+//
+//void sum(int A[5], int B[5]) {//두 일차 배열의 합을 구하는 void함수 
+//    int i;
+//    printf("A+B: ");
+//    for (i = 0; i < 5; i++) {//for문을 이용한 배열의 합 출력.
+//
+//        printf("%d ", A[i] + B[i]);
+//
+//
+//    }
+//    printf("\n");
+//}
+//
+//
+//void mul(int A[5], int B[5]) {// 두 일차 배열의 곱을 구한 void함수 입니다.
+//    int i, j;
+//    printf("A*B: ");
+//
+//    for (i = 0; i < 5; i++) {//for문을 이용한 두 배열의 곱을 구하는 과정.
+//
+//        printf("%d ", A[i] * B[i]);
+//
+//
+//    }
+//    printf("\n");
+//}
